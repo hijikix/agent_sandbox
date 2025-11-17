067fe2951b7ada25b6a5da5b60623a6437cab050
Daisuke Hashimoto <galapagosit@gmail.com>
2025-11-17 15:54:30 +0900

    plan

--- notebooks/plan.py
+++ notebooks/plan.py
@@ -7,7 +7,9 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     import marimo as mo
-    return
+    from itertools import product
+    from rich import print as rprint
+    return mo, product, rprint
 
 
 @app.cell
@@ -19,7 +21,16 @@ def _():
 
 
 @app.cell
+def _(mo):
+    mo.md(r"""
+    ### 色々なパターンでプランニングを行ってみます
+    """)
+    return
+
+
+@app.cell
 def _():
+    # GPT4系のモデル
     GPT4_MODELS = [
         "gpt-4.1-2025-04-14",
     ]
@@ -28,23 +39,25 @@ def _():
 
 @app.cell
 def _():
+    # GPT5系のモデル
     GPT5_MODELS = [
         "gpt-5-nano-2025-08-07",
         # "gpt-5-mini-2025-08-07",
         # "gpt-5-2025-08-07",
     ]
 
+    # APIコール時のパラメータもパターンを用意する
     GPT5_REASONING_EFFORTS = [
         'minimal',
         # 'low',
         # 'medium',
-        'high',
+        # 'high',
     ]
 
     GPT5_TEXT_VERBOSITIES = [
         'low',
         # 'medium',
-        'high',
+        # 'high',
     ]
     return GPT5_MODELS, GPT5_REASONING_EFFORTS, GPT5_TEXT_VERBOSITIES
 
@@ -57,14 +70,15 @@ def _():
     client = OpenAI()
 
     class Plan(BaseModel):
+        """レスポンスの型"""
         tasks: list[str]
-
     return Plan, client
 
 
 @app.cell
 def _(Plan, client):
-    def gen_travel_plan_gpt4(input, model) -> Plan:
+    def gen_plan_gpt4(input, model) -> Plan:
+        """gpt4系のapiコール"""
         response = client.responses.parse(
             model=model,
             input=input,
@@ -72,12 +86,13 @@ def _(Plan, client):
         )
         plan = response.output_parsed
         return plan
-    return (gen_travel_plan_gpt4,)
+    return (gen_plan_gpt4,)
 
 
 @app.cell
 def _(Plan, client):
-    def gen_travel_plan_gpt5(input, model, effort, verbosity) -> Plan:
+    def gen_plan_gpt5(input, model, effort, verbosity) -> Plan:
+        """gpt5系のapiコール"""
         response = client.responses.parse(
             model=model,
             reasoning={
@@ -91,7 +106,15 @@ def _(Plan, client):
         )
         plan = response.output_parsed
         return plan
-    return (gen_travel_plan_gpt5,)
+    return (gen_plan_gpt5,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    ### 情報検索タスクをプランニング（シンプルなシステムプロンプト）
+    """)
+    return
 
 
 @app.cell
@@ -100,47 +123,185 @@ def _(
     GPT5_MODELS,
     GPT5_REASONING_EFFORTS,
     GPT5_TEXT_VERBOSITIES,
-    gen_travel_plan_gpt4,
-    gen_travel_plan_gpt5,
+    gen_plan_gpt4,
+    gen_plan_gpt5,
+    product,
+    rprint,
 ):
-    from itertools import product
-    from rich import print as rprint
+    def _():
+        SYSTEM_PROMPT = """
+    あなたは優秀な情報検索者です。
 
-    input = [
-        {
-            "role": "developer",
-            "content": "あなたは優秀な旅行プランナーです。"
-        },
-        {
-            "role": "user",
-            "content": "伊豆に旅行に行くための計画を立てたいです。プランニングのためのタスクを考えてください。"
-        }
-    ]
+    ユーザの質問に対して情報取得タスクを作成してください。
+    """
+    
+        USER_PROMPT = """
+    ### ユーザの質問
 
-    for model in GPT4_MODELS:
-        plan = gen_travel_plan_gpt4(input, model)
-        rprint({
-            "model": model,
-            "plan": plan,
-        })
+    ドコモとソフトバンクの携帯電話の料金を比較してください。
+    月のデータ使用量は30G程度です。
+    """
+    
+        input = [
+            {
+                "role": "developer",
+                "content": SYSTEM_PROMPT
+            },
+            {
+                "role": "user",
+                "content": USER_PROMPT
+            }
+        ]
+    
+        for model in GPT4_MODELS:
+            plan = gen_plan_gpt4(input, model)
+            rprint({
+                "model": model,
+                "plan": plan,
+            })
+    
+        # 網羅的な組み合わせを生成
+        combinations = list(product(GPT5_MODELS, GPT5_REASONING_EFFORTS, GPT5_TEXT_VERBOSITIES))
+    
+        # 結果を表示
+        for i, (model, effort, verbosity) in enumerate(combinations, 1):
+            plan = gen_plan_gpt5(input, model, effort, verbosity)
+            rprint({
+                "model": model,
+                "effort": effort,
+                "verbosity": verbosity,
+                "plan": plan,
+            })
 
-    # 網羅的な組み合わせを生成
-    combinations = list(product(GPT5_MODELS, GPT5_REASONING_EFFORTS, GPT5_TEXT_VERBOSITIES))
-
-    # 結果を表示
-    for i, (model, effort, verbosity) in enumerate(combinations, 1):
-        plan = gen_travel_plan_gpt5(input, model, effort, verbosity)
-        rprint({
-            "model": model,
-            "effort": effort,
-            "verbosity": verbosity,
-            "plan": plan,
-        })
+    _()
     return
 
 
 @app.cell
 def _():
+    ### 情報検索タスクをプランニング（色々考慮したシステムプロンプト）
+    return
+
+
+@app.cell
+def _(
+    GPT4_MODELS,
+    GPT5_MODELS,
+    GPT5_REASONING_EFFORTS,
+    GPT5_TEXT_VERBOSITIES,
+    gen_plan_gpt4,
+    gen_plan_gpt5,
+    product,
+    rprint,
+):
+    def _():
+        SYSTEM_PROMPT = """
+    あなたは優秀な情報検索者です。
+
+    ユーザの質問に対して情報取得タスクを作成してください。
+    情報取得タスクは別のエージェントが実行し、それぞれ結果を返します。
+    タスクの結果を全て参考にして最終的なユーザへの回答を作成します。
+
+    ### 条件
+    - それぞれの情報検索タスクは独立しているものとし、依存関係は無い様にする。
+    - サブタスクは別のエージェントが実行する。
+    - 出力は1行1タスクでシンプルな文章で、最大でも3行程度にまとめる。
+    """
+    
+        USER_PROMPT = """
+    ### ユーザの質問
+
+    ドコモとソフトバンクの携帯電話の料金を比較してください。
+    月のデータ使用量は30G程度です。
+    """
+    
+        input = [
+            {
+                "role": "developer",
+                "content": SYSTEM_PROMPT
+            },
+            {
+                "role": "user",
+                "content": USER_PROMPT
+            }
+        ]
+    
+        for model in GPT4_MODELS:
+            plan = gen_plan_gpt4(input, model)
+            rprint({
+                "model": model,
+                "plan": plan,
+            })
+    
+        # 網羅的な組み合わせを生成
+        combinations = list(product(GPT5_MODELS, GPT5_REASONING_EFFORTS, GPT5_TEXT_VERBOSITIES))
+    
+        # 結果を表示
+        for i, (model, effort, verbosity) in enumerate(combinations, 1):
+            plan = gen_plan_gpt5(input, model, effort, verbosity)
+            rprint({
+                "model": model,
+                "effort": effort,
+                "verbosity": verbosity,
+                "plan": plan,
+            })
+
+    _()
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    ### 旅行計画タスクをプランニング
+    """)
+    return
+
+
+@app.cell
+def _(
+    GPT4_MODELS,
+    GPT5_MODELS,
+    GPT5_REASONING_EFFORTS,
+    GPT5_TEXT_VERBOSITIES,
+    gen_plan_gpt4,
+    gen_plan_gpt5,
+    product,
+    rprint,
+):
+    def _():
+        input = [
+            {
+                "role": "developer",
+                "content": "あなたは優秀な旅行プランナーです。"
+            },
+            {
+                "role": "user",
+                "content": "伊豆に旅行に行くための計画を立てたいです。プランニングのためのタスクを考えてください。"
+            }
+        ]
+    
+        for model in GPT4_MODELS:
+            plan = gen_plan_gpt4(input, model)
+            rprint({
+                "model": model,
+                "plan": plan,
+            })
+    
+        # 網羅的な組み合わせを生成
+        combinations = list(product(GPT5_MODELS, GPT5_REASONING_EFFORTS, GPT5_TEXT_VERBOSITIES))
+    
+        # 結果を表示
+        for i, (model, effort, verbosity) in enumerate(combinations, 1):
+            plan = gen_plan_gpt5(input, model, effort, verbosity)
+            rprint({
+                "model": model,
+                "effort": effort,
+                "verbosity": verbosity,
+                "plan": plan,
+            })
+
+    _()
     return
 
 
