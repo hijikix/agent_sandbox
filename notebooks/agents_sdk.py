715d925f7ee7d53fe526a9fdbedc4089f7c0a5ce
Daisuke Hashimoto <galapagosit@gmail.com>
2025-11-21 18:11:04 +0900

    agents_sdk.py

--- notebooks/agents_sdk.py
+++ notebooks/agents_sdk.py
@@ -7,9 +7,9 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     import marimo as mo
-    from agents import Agent, Runner
+    from agents import Agent, Runner, function_tool
     from rich import print as rprint
-    return Agent, Runner, mo
+    return Agent, Runner, function_tool, mo, rprint
 
 
 @app.cell
@@ -29,10 +29,31 @@ def _(mo):
 
 
 @app.cell
-async def _(Agent, Runner):
-    agent = Agent(name="Assistant", instructions="You are a helpful assistant")
+async def _(Agent, Runner, function_tool, rprint):
+    @function_tool
+    def search_documents(keywords: str) -> list[str]:
+        if '国別料金表3' in keywords:
+            response = "アンギラの国際通話料金は9999円/1分です。"
+        elif 'アンギラ' in keywords or "Angila" in keywords or "Angira" in keywords:
+            response = "アンギラの国際通話料金は国別料金表3に記載されています。"
+        else:
+            response = "情報が見つかりませんでした。"
 
-    result = await Runner.run(agent, "Write a haiku about recursion in programming.")
+        print(">>> search_documents called")
+        rprint({
+            'keywords': keywords,
+            "response": response
+        })
+        return [response]
+
+    agent = Agent(
+        name="Hello world",
+        instructions="You are a helpful agent.",
+        tools=[search_documents],
+    )
+
+
+    result = await Runner.run(agent, input="アンギラの国際通話の料金をドキュメントデータベースから検索して教えてください")
     print(result.final_output)
     return
 
